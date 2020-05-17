9f028bd7cb804427456c55b30cbc5613cae3ad45
Androz2091 <androz2091@gmail.com>
2020-05-17 10:26:39 +0200

    fix: use res#render() instead of res#redirect()

--- middlewares/fetchPayment.js
+++ middlewares/fetchPayment.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
     if(!req.member) return res.render("error");
     // Récupération du paiement en question
     req.payment = req.member.find((p) => p.id === req.params.paymentID);
-    if(!req.payment) return res.redirect("error");
+    if(!req.payment) return res.render("error");
     if(req.payment.paid) req.query.error = "already_paid";
     return next();
 };
